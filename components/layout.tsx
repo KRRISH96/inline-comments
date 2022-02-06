a11633d24aa361f6537119fb14c0c09416b05079
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2022-02-06 23:58:03 +0530

    use sample screenshot for the og-image

--- components/layout.tsx
+++ components/layout.tsx
@@ -27,13 +27,11 @@ export default function Layout({
 				<link rel="icon" href="/favicon.ico" />
 				<meta
 					name="description"
-					content="Medium / Dropbox paper line inline comments"
+					content="Medium / Dropbox Paper like inline comments"
 				/>
 				<meta
 					property="og:image"
-					content={`https://og-image.vercel.app/${encodeURI(
-						siteTitle
-					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+					content="/images/sample_ss.png"
 				/>
 				<meta name="og:title" content={siteTitle} />
 				<meta name="twitter:card" content="summary_large_image" />
